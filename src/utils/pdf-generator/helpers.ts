c2d9eaf26460aa359d3e05b1a2e5d0362f7570a4
Kaspyy <50270928+Kaspyy@users.noreply.github.com>
2022-11-09 13:30:05 +0100

    create invoice template

--- src/utils/pdf-generator/helpers.ts
+++ src/utils/pdf-generator/helpers.ts
@@ -27,17 +27,151 @@ function generateCustomerInformation(
   doc: PDFKit.PDFDocument,
   invoice: Invoice
 ) {
-  const shipping = invoice.shipping;
+  doc.fillColor('#444444').fontSize(20).text('Invoice', 50, 160);
+
+  generateHr(doc, 185);
+
+  const customerInformationTop = 200;
 
   doc
-    .text(`Invoice Number: ${invoice.invoice_nr}`, 50, 200)
-    .text(`Invoice Date: ${new Date()}`, 50, 215)
-    .text(`Balance Due: ${invoice.subtotal - invoice.paid}`, 50, 130)
+    .fontSize(10)
+    .text('Invoice Number:', 50, customerInformationTop)
+    .font('Helvetica-Bold')
+    .text(invoice.invoice_nr.toString(), 150, customerInformationTop)
+    .font('Helvetica')
+    .text('Invoice Date:', 50, customerInformationTop + 15)
+    .text(formatDate(new Date()), 150, customerInformationTop + 15)
+    .text('Balance Due:', 50, customerInformationTop + 30)
+    .text(
+      formatCurrency(invoice.subtotal - invoice.paid),
+      150,
+      customerInformationTop + 30
+    )
 
-    .text(shipping.name, 300, 200)
-    .text(shipping.address, 300, 215)
-    .text(`${shipping.city}, ${shipping.state}, ${shipping.country}`, 300, 130)
+    .font('Helvetica-Bold')
+    .text(invoice.shipping.name, 300, customerInformationTop)
+    .font('Helvetica')
+    .text(invoice.shipping.address, 300, customerInformationTop + 15)
+    .text(
+      invoice.shipping.city +
+        ', ' +
+        invoice.shipping.state +
+        ', ' +
+        invoice.shipping.country,
+      300,
+      customerInformationTop + 30
+    )
     .moveDown();
+
+  generateHr(doc, 252);
 }
 
-export { generateHeader, generateFooter, generateCustomerInformation };
+function generateTableRow(
+  doc: PDFKit.PDFDocument,
+  y: number,
+  c1: string,
+  c2: string,
+  c3: string,
+  c4: string,
+  c5: string
+) {
+  doc
+    .fontSize(10)
+    .text(c1, 50, y)
+    .text(c2, 150, y)
+    .text(c3, 280, y, { width: 90, align: 'right' })
+    .text(c4, 370, y, { width: 90, align: 'right' })
+    .text(c5, 0, y, { align: 'right' });
+}
+
+function generateInvoiceTable(doc: PDFKit.PDFDocument, invoice) {
+  let i: number;
+  const invoiceTableTop = 330;
+
+  doc.font('Helvetica-Bold');
+  generateTableRow(
+    doc,
+    invoiceTableTop,
+    'Item',
+    'Description',
+    'Unit Cost',
+    'Quantity',
+    'Line Total'
+  );
+  generateHr(doc, invoiceTableTop + 20);
+  doc.font('Helvetica');
+
+  for (i = 0; i < invoice.items.length; i++) {
+    const item = invoice.items[i];
+    const position = invoiceTableTop + (i + 1) * 30;
+    generateTableRow(
+      doc,
+      position,
+      item.item,
+      item.description,
+      formatCurrency(item.amount / item.quantity),
+      item.quantity,
+      formatCurrency(item.amount)
+    );
+
+    generateHr(doc, position + 20);
+  }
+
+  const subtotalPosition = invoiceTableTop + (i + 1) * 30;
+  generateTableRow(
+    doc,
+    subtotalPosition,
+    '',
+    '',
+    'Subtotal',
+    '',
+    formatCurrency(invoice.subtotal)
+  );
+
+  const paidToDatePosition = subtotalPosition + 20;
+  generateTableRow(
+    doc,
+    paidToDatePosition,
+    '',
+    '',
+    'Paid To Date',
+    '',
+    formatCurrency(invoice.paid)
+  );
+
+  const duePosition = paidToDatePosition + 25;
+  doc.font('Helvetica-Bold');
+  generateTableRow(
+    doc,
+    duePosition,
+    '',
+    '',
+    'Balance Due',
+    '',
+    formatCurrency(invoice.subtotal - invoice.paid)
+  );
+  doc.font('Helvetica');
+}
+
+function generateHr(doc: PDFKit.PDFDocument, y: number) {
+  doc.strokeColor('#aaaaaa').lineWidth(1).moveTo(50, y).lineTo(560, y).stroke();
+}
+
+function formatCurrency(cents: number) {
+  return '$' + (cents / 100).toFixed(2);
+}
+
+function formatDate(date: Date) {
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  return year + '/' + month + '/' + day;
+}
+
+export {
+  generateHeader,
+  generateFooter,
+  generateCustomerInformation,
+  generateInvoiceTable,
+};
